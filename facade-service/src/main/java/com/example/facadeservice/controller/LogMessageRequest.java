da9342e480d234f3144a6c52d05a53f59e810da8
Andrii Ahitoliev <andrei.agitolyev@gmail.com>
2025-02-17 00:21:07 +0200

    fixed error

---
+++ facade-service/src/main/java/com/example/facadeservice/controller/LogMessageRequest.java
@@ -0,0 +1,33 @@
+package com.example.facadeservice.controller;
+
+import java.util.UUID;
+
+public class LogMessageRequest {
+    private UUID id;
+    private String message;
+
+    public LogMessageRequest() {
+    }
+
+    public LogMessageRequest(UUID id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
